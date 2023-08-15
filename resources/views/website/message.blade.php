e6acb8f92ec58d1dfc20cea1da2f23f78b100a42
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-08-15 16:35:07 +0700

    update message code

--- resources/views/website/message.blade.php
+++ resources/views/website/message.blade.php
@@ -559,13 +559,9 @@
                             </div>
                         </div>
                         <div class="incoming_msg">
-                            <div class="incoming_msg_img">
-                                <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" />
-                            </div>
                             <div class="received_msg">
                                 <div class="received_withd_msg">
-                                    <p>What's going on ?</p>
-                                    <span class="time_date"> 11:01 AM | Today</span>
+                                    <br />
                                 </div>
                             </div>
                         </div>
