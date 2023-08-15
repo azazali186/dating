939f5ac12370920fdd632bfe635a2821ca8dcacf
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-08-15 17:05:08 +0700

    update message code

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -16,10 +16,10 @@ class MessageController extends Controller
         }
         $role = Session::get('sessdata')['role'];
         if ($role == 'user') {
-            $messages = Message::with(['user', 'seller'])->groupBy('seller_id')->orderBy('created_at', 'desc')->where('user_id', Session::get('sessdata')['id'])->get();
+            $messages = Message::with(['user', 'seller'])->groupBy('seller_id')->orderBy('created_at', 'asc')->where('user_id', Session::get('sessdata')['id'])->get();
         }
         if ($role == 'seller') {
-            $messages = Message::with(['user', 'seller'])->groupBy('user_id')->orderBy('created_at', 'desc')->where('seller_id', Session::get('sessdata')['id'])->get();
+            $messages = Message::with(['user', 'seller'])->groupBy('user_id')->orderBy('created_at', 'asc')->where('seller_id', Session::get('sessdata')['id'])->get();
         }
 
         return view('website.message', compact('messages', 'role'));
