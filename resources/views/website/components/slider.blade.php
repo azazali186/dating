d3b2272855ccc25afd3b80fd3ea7fa6235529690
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-08-15 09:50:18 +0700

    index changed

--- resources/views/website/components/slider.blade.php
+++ resources/views/website/components/slider.blade.php
@@ -1,27 +1,29 @@
-<section class="hero bg_img">
-    <div id="demo" class="carousel slide" data-ride="carousel">
-        <ul class="carousel-indicators">
-            <li data-target="#demo" data-slide-to="0" class="active"></li>
-            <li data-target="#demo" data-slide-to="1"></li>
-            <li data-target="#demo" data-slide-to="2"></li>
-        </ul>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="https://ccasinize.co/assets/images/slider/khmer%20game%20culture%20(1).png" />
-            </div>
-            <div class="carousel-item">
-                <img src="https://ccasinize.co/assets/images/slider/khmer%20game%20culture%20(1).png" />
-            </div>
-            <div class="carousel-item">
-                <img src="https://ccasinize.co/assets/images/slider/khmer%20game%20culture%20(1).png" />
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#demo" data-slide="prev">
-            <span class="carousel-control-prev-icon"></span>
-        </a>
-        <a class="carousel-control-next" href="#demo" data-slide="next">
-            <span class="carousel-control-next-icon"></span>
-        </a>
+<div id="demo" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
     </div>
-</section>
-<script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="https://ccasinize.co/assets/images/slider/khmer%20game%20culture%20(1).png" alt="Los Angeles"
+                class="d-block" style="width: 100%;" />
+        </div>
+        <div class="carousel-item ">
+            <img src="https://ccasinize.co/assets/images/slider/khmer%20game%20culture%20(1).png" alt="Los Angeles"
+                class="d-block" style="width: 100%;" />
+        </div>
+        <div class="carousel-item ">
+            <img src="https://ccasinize.co/assets/images/slider/khmer%20game%20culture%20(1).png" alt="Los Angeles"
+                class="d-block" style="width: 100%;" />
+        </div>
+    </div>
+
+    <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+    </button>
+</div>
